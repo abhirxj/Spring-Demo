36c74d6928bb40e70ead30a8bd0e75fc421473f0
Abhiraj Singh Chouhan <abhirajchouhan018@gmail.com>
2026-02-13 13:31:15 +0530

    just App file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,8 +12,14 @@ public class App
     public static void main( String[] args ) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Dev obj = (Dev)context.getBean("dev");
-//        obj.build();
+            //Dev obj = (Dev)context.getBean("dev");
+            //or use this
+            Dev ob = context.getBean(Dev.class);
+            ob.build();
+
+            //System.out.println(obj.getBranch());
+            //System.out.println(obj.getAge());
+
 
     }
 
